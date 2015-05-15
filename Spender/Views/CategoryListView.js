ad808a7c902a03b22a408662ce187843f2cc9493
Wim Maesschalck <wim.maesschalck@cozmos.be>
2015-05-15 07:34:29 +0200

    Implemented simple list view component.

--- Spender/Views/CategoryListView.js
+++ Spender/Views/CategoryListView.js
@@ -2,37 +2,61 @@
 
 var React = require('react-native');
 var {
+  ListView,
+  TouchableHighlight,
   StyleSheet,
   Text,
   View,
 } = React;
 
+
 var CategoryListView = React.createClass({
+ 
 
-    getInitialState: function(){
-      return {amount : 10}
-    },
+  getInitialState: function() {
+    var ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
+    return {
+     dataSource: ds.cloneWithRows(['row 1', 'row 2','row 1', 'row 2','row 1', 'row 2','row 1', 'row 2','row 1', 'row 2','row 1', 'row 2','row 1', 'row 2','row 1', 'row 2','row 1', 'row 2','row 1', 'row 2']),
+    };
+  },
 
-    render: function() {
+  render: function() {
     return (
-      <View style={styles.container}>
-         <Text>Building a list view here</Text>
-      </View>
-     );
-    } 
+      <ListView dataSource={this.state.dataSource}
+                renderRow={(rowData) => (
+                  <View style={styles.rowContainer}>
+                    <View style={styles.row}>
+                      <Text style={styles.text}>{rowData}</Text>
+                   </View>
+                  <View style={styles.separator}></View>
+                  </View>
+                  )
+                }
+      />
+    );
+  },
+  
 });
 
 var styles = StyleSheet.create({
-  container: {
+  rowContainer: {
+    flex:1,
+    backgroundColor: '#99EE99',
+    alignSelf: 'stretch'
+  },
+  row: {
     flex: 1,
-    alignSelf: 'stretch',
-    justifyContent: 'flex-start',
-    backgroundColor: '#00FFAA',
-    marginTop: 20,
-    paddingTop: 20,
-    paddingLeft: 0,
-    paddingRight: 0
-  }
+    padding: 10,
+    backgroundColor: '#CCEEFF',
+    alignItems: 'flex-start'
+  },
+  separator: {
+    height: 1,
+    backgroundColor: '#CCCCCC',
+  },
+  text: {
+    backgroundColor: '#DEaaFE'
+  },
 });
 
 module.exports = CategoryListView;
